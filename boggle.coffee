581e4c2a0e2cdd0a437c62d31ea1bce96089d80e
Steve Howell <steve@strings.com>
2011-06-30 23:01:22 -0700

    shaking dice

--- boggle.coffee
+++ boggle.coffee
@@ -3,14 +3,23 @@ boggle = ->
   num_squares = size * size
   board = ->
     table = $("<table border=1>")
+    n = 0
     for i in [0...5]
       tr = $("<tr>")
       table.append(tr)
       for j in [0...5]
-        td = $("<td>x#{i},#{j}</td>")
+        td = $("<td>x#{i},#{j},#{n}</td>")
+        td.attr("id", "pos#{n}")
+        n += 1
         tr.append(td)
     $("#board").append(table)    
   board()
+  shake_dice_onto_board = ->
+    numbers = [0...num_squares]
+    dice = _.sortBy(numbers, Math.random)
+    for i in [0...num_squares]
+      $("#pos#{i}").html(dice[i])
+  shake_dice_onto_board()
   
 jQuery(document).ready ->
   boggle()
--- boggle.js
+++ boggle.js
@@ -1,23 +1,41 @@
 (function() {
   var boggle;
   boggle = function() {
-    var board, num_squares, size;
+    var board, num_squares, shake_dice_onto_board, size;
     size = 5;
     num_squares = size * size;
     board = function() {
-      var i, j, table, td, tr;
+      var i, j, n, table, td, tr;
       table = $("<table border=1>");
+      n = 0;
       for (i = 0; i < 5; i++) {
         tr = $("<tr>");
         table.append(tr);
         for (j = 0; j < 5; j++) {
-          td = $("<td>x" + i + "," + j + "</td>");
+          td = $("<td>x" + i + "," + j + "," + n + "</td>");
+          td.attr("id", "pos" + n);
+          n += 1;
           tr.append(td);
         }
       }
       return $("#board").append(table);
     };
-    return board();
+    board();
+    shake_dice_onto_board = function() {
+      var dice, i, numbers, _i, _results, _results2;
+      numbers = (function() {
+        _results = [];
+        for (var _i = 0; 0 <= num_squares ? _i < num_squares : _i > num_squares; 0 <= num_squares ? _i += 1 : _i -= 1){ _results.push(_i); }
+        return _results;
+      }).apply(this, arguments);
+      dice = _.sortBy(numbers, Math.random);
+      _results2 = [];
+      for (i = 0; (0 <= num_squares ? i < num_squares : i > num_squares); (0 <= num_squares ? i += 1 : i -= 1)) {
+        _results2.push($("#pos" + i).html(dice[i]));
+      }
+      return _results2;
+    };
+    return shake_dice_onto_board();
   };
   jQuery(document).ready(function() {
     return boggle();
