87931772ba979ab99ec4b03f70d7a1b3b6f08b3c
Steve Howell <steve@strings.com>
2011-06-30 23:10:29 -0700

    refactoring

--- boggle.coffee
+++ boggle.coffee
@@ -2,23 +2,28 @@ boggle = ->
   size = 5
   num_squares = size * size
   board = ->
-    table = $("<table border=1>")
-    n = 0
-    for i in [0...5]
-      tr = $("<tr>")
-      table.append(tr)
-      for j in [0...5]
-        td = $("<td>x#{i},#{j},#{n}</td>")
-        td.attr("id", "pos#{n}")
-        n += 1
-        tr.append(td)
-    $("#board").append(table)    
-  board()
+    do ->
+      table = $("<table border=1>")
+      n = 0
+      for i in [0...5]
+        tr = $("<tr>")
+        table.append(tr)
+        for j in [0...5]
+          td = $("<td>x#{i},#{j},#{n}</td>")
+          td.attr("id", "pos#{n}")
+          n += 1
+          tr.append(td)
+      $("#board").append(table)
+    self =
+      place_die: (i, value) ->
+        $("#pos#{i}").html(value)
+  b = board()
+
   shake_dice_onto_board = ->
     numbers = [0...num_squares]
     dice = _.sortBy(numbers, Math.random)
     for i in [0...num_squares]
-      $("#pos#{i}").html(dice[i])
+      b.place_die(i, dice[i])
   shake_dice_onto_board()
   
 jQuery(document).ready ->
--- boggle.js
+++ boggle.js
@@ -1,26 +1,34 @@
 (function() {
   var boggle;
   boggle = function() {
-    var board, num_squares, shake_dice_onto_board, size;
+    var b, board, num_squares, shake_dice_onto_board, size;
     size = 5;
     num_squares = size * size;
     board = function() {
-      var i, j, n, table, td, tr;
-      table = $("<table border=1>");
-      n = 0;
-      for (i = 0; i < 5; i++) {
-        tr = $("<tr>");
-        table.append(tr);
-        for (j = 0; j < 5; j++) {
-          td = $("<td>x" + i + "," + j + "," + n + "</td>");
-          td.attr("id", "pos" + n);
-          n += 1;
-          tr.append(td);
+      var self;
+      (function() {
+        var i, j, n, table, td, tr;
+        table = $("<table border=1>");
+        n = 0;
+        for (i = 0; i < 5; i++) {
+          tr = $("<tr>");
+          table.append(tr);
+          for (j = 0; j < 5; j++) {
+            td = $("<td>x" + i + "," + j + "," + n + "</td>");
+            td.attr("id", "pos" + n);
+            n += 1;
+            tr.append(td);
+          }
         }
-      }
-      return $("#board").append(table);
+        return $("#board").append(table);
+      })();
+      return self = {
+        place_die: function(i, value) {
+          return $("#pos" + i).html(value);
+        }
+      };
     };
-    board();
+    b = board();
     shake_dice_onto_board = function() {
       var dice, i, numbers, _i, _results, _results2;
       numbers = (function() {
@@ -31,7 +39,7 @@
       dice = _.sortBy(numbers, Math.random);
       _results2 = [];
       for (i = 0; (0 <= num_squares ? i < num_squares : i > num_squares); (0 <= num_squares ? i += 1 : i -= 1)) {
-        _results2.push($("#pos" + i).html(dice[i]));
+        _results2.push(b.place_die(i, dice[i]));
       }
       return _results2;
     };
